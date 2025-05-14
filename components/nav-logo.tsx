832cfeb18e7709c5be1e6eaaa42ff482d462f5fe
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-05-14 07:08:12 +0530

    1. initial Structure

---
+++ components/nav-logo.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+
+const NavLogo = ({branding}:{branding: {
+    logo: string
+    name: string
+    tagline: string
+}}) => {
+  return (
+       <div className="flex items-center space-x-2">
+            <Image src={branding.logo}  alt="logo" width={50} height={50}/>
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-bold">{branding.name}</span>
+            <span className="truncate text-xs">{branding.tagline}</span>
+          </div>
+        </div>
+  )
+}
+
+export default NavLogo
