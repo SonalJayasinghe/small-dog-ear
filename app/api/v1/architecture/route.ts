a949c4f6dc354fafe4426761f9bc75bec70dd8a2
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-05-20 10:33:59 +0530

    1. default architecture router refactored

--- app/api/v1/architecture/route.ts
+++ app/api/v1/architecture/route.ts
@@ -2,13 +2,14 @@ import connectMongo from "@/lib/mongoose";
 import { ArchitectureSchema } from "@/lib/schema";
 import Architecture from "@/models/architecture";
 import mongoose from "mongoose";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     try {
         await connectMongo();
     }
     catch (error) {
-        return new Response("MongoDB connection failed.", { status: 500 });
+        return NextResponse.json({ error: "MongoDB connection failed." }, { status: 500 });
     }
 
     let body: any;
@@ -16,45 +17,52 @@ export async function POST(req: Request) {
         body = await req.json();
     }
     catch (error) {
-        return new Response("Invalid JSON", { status: 400 });
+        return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
     }
 
 
     const parse = ArchitectureSchema.safeParse(body);
     if (!parse.success) {
-        return new Response(JSON.stringify(parse.error), { status: 400 });
+        return NextResponse.json({ error: parse.error }, { status: 400 });
     }
 
 
     try {
-        const doc = await Architecture.create(parse.data);
-        return new Response("Architecture created successfully.", { status: 201 });
+        await Architecture.create(parse.data);
+        return NextResponse.json({ message: "Architecture created successfully." }, { status: 201 });
     }
     catch (error: any) {
         if (error.code === 11000) {
-            return new Response("Architecture already exists.", { status: 409 });
+            return NextResponse.json({ error: "Architecture already exists" }, { status: 409 });
         }
 
         if (error instanceof mongoose.Error.ValidationError) {
-            return new Response("Validation error: " + error.message, { status: 422 });
+            return NextResponse.json({ error: error.message }, { status: 422 });
         }
-        return new Response("Failed to create architecture.", { status: 500 });
+        return NextResponse.json({ error: "Failed to create architecture." }, { status: 500 });
     }
 }
 
 
-export async function GET(req: Request){
-    try{
+export async function GET(req: Request) {
+    try {
         await connectMongo();
     }
     catch (error) {
-        return new Response("MongoDB connection failed.", { status: 500 });
+        return NextResponse.json({ error: "MongoDB connection failed." }, { status: 500 });
     }
 
-    const architectures = await Architecture.find({type: "default"}).sort({ createdAt: -1 }).exec();
-    if (architectures.length === 0) {
-        return new Response("No architectures found.", { status: 404 });
+    const doc = await Architecture.find({ type: "default" }).sort({ createdAt: -1 }).exec();
+    if (doc.length === 0) {
+        return NextResponse.json({ error: "Architecture not found." }, { status: 404 });
     }
-    return new Response(JSON.stringify(architectures), { status: 200 });
+
+    const response = doc.map((item) => ({
+        name: item.name,
+        sections: item.sections,
+        type: item.type,
+    }))
+
+    return NextResponse.json(response, { status: 200 });
 }
 
