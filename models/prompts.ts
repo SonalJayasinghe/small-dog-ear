ea5d8d01bfaf409dad003343ca7f767eaaf73106
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-05-20 20:54:39 +0530

    1. Saved prompt structure created

---
+++ models/prompts.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const promptSchema = new Schema({
+  promptName: String,
+  promptDescription: String,
+  prompt: [
+    {
+        sectionName: String,
+        sectionPrompt: String,
+        subsections: [
+            {
+                subsectionName: String,
+                subsectionPrompt: String
+            }
+        ]
+    }
+  ],
+  userId: String
+});
+
+const Prompt = models.Prompt || mongoose.model("Prompt", promptSchema);
+export default Prompt;
