dc34504d65e8cfb2f0715dd9fed1c176fe799f18
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-05-21 10:56:49 +0530

    1. dark mode created 2. login integrated

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -4,14 +4,22 @@ import { ReactNode, useState } from 'react'
 import { NextAuthProvider } from './auth-provider'
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/app-sidebar'
+import { ThemeProvider } from 'next-themes'
 
 export default function Providers({ children }: { children: ReactNode }) {
 
   return (
     <NextAuthProvider>
+                <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
             <main>
               {children}
             </main>
+            </ThemeProvider>
     </NextAuthProvider>
   )
 }
