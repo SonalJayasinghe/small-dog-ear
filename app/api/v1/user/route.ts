95a9084599c2898ac6dbb6044a312acd920fb584
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-05-27 06:54:02 +0530

    1. unnesesessay imports removed

--- app/api/v1/user/route.ts
+++ app/api/v1/user/route.ts
@@ -1,6 +1,5 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
-import type { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 // export default async function handler(req: NextApiRequest, res: NextApiResponse) {
