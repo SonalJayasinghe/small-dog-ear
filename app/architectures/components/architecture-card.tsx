c969079c63d54b2ee86f1dc1bb0d46b7ab7e0255
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-05-28 05:33:54 +0530

    1. architecture card updated

--- app/architectures/components/architecture-card.tsx
+++ app/architectures/components/architecture-card.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardHeader,
@@ -7,25 +8,35 @@ import {
   CardAction,
 } from "@/components/ui/card";
 import { Architecture } from "@/lib/schema";
+import { cn } from "@/lib/utils";
+import { IconTrash } from "@tabler/icons-react";
 import React from "react";
 
 const ArchitectureCard = ({ data }: { data: Architecture }) => {
   return (
     <>
-      <Card className="@container/card cursor-pointer">
-        <CardHeader>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+      <div
+        className={cn(
+          "bg-card text-card-foreground flex flex-col gap-3 rounded-xl border py-4 px-4 shadow-sm h-full justify-between cursor-pointer"
+        )}
+      >
+        <div className=" flex w-full">
+          <p className="text-xl font-semibold line-clamp-1 gap-2">
             {data.name}
-          </CardTitle>
-          <CardDescription>{data.description}</CardDescription>
-
-          <CardAction>
-            <Badge variant={"secondary"}>
-              {data.type.toLocaleUpperCase()}
-            </Badge>
-          </CardAction>
-        </CardHeader>
-      </Card>
+          </p>
+        </div>
+        <div className="w-full flex">
+          <p className=" text-muted-foreground text-sm line-clamp-3">
+            {data.description}
+          </p>
+        </div>
+        <div className=" flex w-full justify-between items-end">
+          <Badge variant={"outline"} className="font-light">
+            {data.type.charAt(0).toUpperCase() + data.type.slice(1)}
+          </Badge>
+          {data.type === 'custom' && <Button className="w-6 h-6" variant={"destructive"}> <IconTrash/> </Button>}
+        </div>
+      </div>
     </>
   );
 };
--- app/architectures/page.tsx
+++ app/architectures/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import AddArchitectureForm from "@/components/forms/add-architecture-form";
 import React from "react";
 import axios from "axios";
@@ -8,40 +8,39 @@ import { toast } from "sonner";
 import ArchitectureCard from "./components/architecture-card";
 import { Architecture } from "@/lib/schema";
 
-
 export default function Page() {
+  const [architectures, setArchitectuers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const [architectures, setArchitectuers] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(()=>{
-
-      axios.get('/api/v1/architecture')
-        .then((res) => {
-          setArchitectuers(res.data)
-          setLoading(false)
-          console.log(res.data)
-        })
-        .catch((err) => {
-          setLoading(false);
-          toast("Something Went Wrong")
-        })
-    
-  },[])
-
+  useEffect(() => {
+    axios
+      .get("/api/v1/architecture")
+      .then((res) => {
+        setArchitectuers(res.data);
+        setLoading(false);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        toast("Something Went Wrong");
+      });
+  }, []);
 
   return (
     <div className="flex flex-1 flex-col">
-      
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
           <div className="px-4 lg:px-6"></div>
-          <AddArchitectureForm/>
+          <AddArchitectureForm />
         </div>
         {loading && <p> loading</p>}
-       {architectures.map((data: Architecture, index: number) => (
-  <ArchitectureCard key={index} data={data}/>
-))}
+    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+          {architectures.map((data: Architecture, index: number) => (
+            <div className="w-full" key={index}>
+              <ArchitectureCard data={data} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
