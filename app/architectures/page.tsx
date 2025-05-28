f960485dc75e817a71b962cc36005d65b1f370dc
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-05-28 06:11:41 +0530

    1. page updated

--- app/architectures/page.tsx
+++ app/architectures/page.tsx
@@ -7,12 +7,13 @@ import { useSession } from "next-auth/react";
 import { toast } from "sonner";
 import ArchitectureCard from "./components/architecture-card";
 import { Architecture } from "@/lib/schema";
+import AddNewArchitecture from "./components/add-new-architecture";
 
 export default function Page() {
   const [architectures, setArchitectuers] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
+  const fetchArchitectures = () => {
     axios
       .get("/api/v1/architecture")
       .then((res) => {
@@ -24,6 +25,9 @@ export default function Page() {
         setLoading(false);
         toast("Something Went Wrong");
       });
+  };
+  useEffect(() => {
+    fetchArchitectures();
   }, []);
 
   return (
@@ -31,10 +35,10 @@ export default function Page() {
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
           <div className="px-4 lg:px-6"></div>
-          <AddArchitectureForm />
+          <AddNewArchitecture onAdd ={fetchArchitectures}/>
         </div>
         {loading && <p> loading</p>}
-    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+        <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
           {architectures.map((data: Architecture, index: number) => (
             <div className="w-full" key={index}>
               <ArchitectureCard data={data} />
