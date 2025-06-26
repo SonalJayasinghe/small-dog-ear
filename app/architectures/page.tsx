78069c5235fe18ae520a102c0137f2c54731fb31
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-06-26 22:12:58 +0530

    architecture page updated

--- app/architectures/page.tsx
+++ app/architectures/page.tsx
@@ -1,15 +1,18 @@
 "use client";
-import AddArchitectureForm from "@/components/forms/add-architecture-form";
+
+
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
 import { toast } from "sonner";
 import ArchitectureCard from "./components/architecture-card";
 import { Architecture } from "@/lib/schema";
-import AddNewArchitecture from "./components/add-new-architecture";
+import { useRouter } from "next/navigation";
+import Spinenr from "@/components/spinner";
+
 
 export default function Page() {
+  const router = useRouter();
   const [architectures, setArchitectuers] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -34,17 +37,29 @@ export default function Page() {
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <div className="px-4 lg:px-6"></div>
-          <AddNewArchitecture onAdd ={fetchArchitectures}/>
         </div>
-        {loading && <p> loading</p>}
+
+        {loading? <Spinenr/> :
+
         <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
           {architectures.map((data: Architecture, index: number) => (
             <div className="w-full" key={index}>
               <ArchitectureCard data={data} />
             </div>
           ))}
+          <div className="w-full">
+            <div 
+            onClick={ ()=> { router.push("/architectures/add-new")}}
+            className="border-2 border-dashed border-gray-400 rounded-xl p-6 flex items-center justify-center cursor-pointer h-full hover:bg-gray-50">
+              <div className="text-center">
+                <p className="text-lg font-semibold text-gray-600">
+                  + Add New Architecture
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+}
       </div>
     </div>
   );
