7b741c1e27d2b9c58d29477edfc7557891d7cdf0
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-06-26 22:52:19 +0530

    1. breadcrumbs added for architecture page

--- app/architectures/page.tsx
+++ app/architectures/page.tsx
@@ -9,7 +9,8 @@ import ArchitectureCard from "./components/architecture-card";
 import { Architecture } from "@/lib/schema";
 import { useRouter } from "next/navigation";
 import Spinenr from "@/components/spinner";
-
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import Link from "next/link";
 
 export default function Page() {
   const router = useRouter();
@@ -36,12 +37,28 @@ export default function Page() {
   return (
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
-        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-        </div>
+        {/* <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        </div> */}
+
+        <div className="flex flex-col pl-8 pt-2">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link href="/dashboard">Home</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Architecture</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
 
         {loading? <Spinenr/> :
 
-        <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+        <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 pt-2">
           {architectures.map((data: Architecture, index: number) => (
             <div className="w-full" key={index}>
               <ArchitectureCard data={data} />
