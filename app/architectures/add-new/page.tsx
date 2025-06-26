a3d4af1e7c3867560494b4599ac1c9bf1383b6a4
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-06-26 22:13:20 +0530

    1. dedicated page added for add architecture

--- app/architectures/add-new/page.tsx
+++ app/architectures/add-new/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import AddArchitectureForm from "@/components/forms/add-architecture-form";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="p-8">
+      <div className="p-4 ring-1 ring-accent shadow-sm rounded-xl">
+        <AddArchitectureForm />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
