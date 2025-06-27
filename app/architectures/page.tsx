2c787040856b20787712bceb66c7e74ac0936b2f
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-06-27 20:38:07 +0530

    1.minor update

--- app/architectures/page.tsx
+++ app/architectures/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -9,8 +8,24 @@ import ArchitectureCard from "./components/architecture-card";
 import { Architecture } from "@/lib/schema";
 import { useRouter } from "next/navigation";
 import Spinenr from "@/components/spinner";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import Link from "next/link";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function Page() {
   const router = useRouter();
@@ -41,42 +56,62 @@ export default function Page() {
         </div> */}
 
         <div className="flex flex-col pl-8 pt-2">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-                <Link href="/dashboard">Home</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Architectures</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-      </div>
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href="/dashboard">Home</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Architectures</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
 
-        {loading? <Spinenr/> :
-
-        <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 pt-2">
-          {architectures.map((data: Architecture, index: number) => (
-            <div className="w-full" key={index}>
-              <ArchitectureCard data={data} onDelete={fetchArchitectures} />
-            </div>
-          ))}
-          <div className="w-full">
-            <div 
-            onClick={ ()=> { router.push("/architectures/add-new")}}
-            className="border-2 border-dashed border-gray-400 rounded-xl p-6 flex items-center justify-center cursor-pointer h-full hover:bg-gray-50">
-              <div className="text-center">
-                <p className="text-lg font-semibold text-gray-600">
-                  + Add New Architecture
-                </p>
+        {loading ? (
+          <Spinenr />
+        ) : (
+          <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 pt-2">
+            {architectures.map((data: Architecture, index: number) => (
+              <Dialog key={index}>
+                <DialogTrigger asChild>
+                  <div className="w-full">
+                    <ArchitectureCard
+                      data={data}
+                      onDelete={fetchArchitectures}
+                    />
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>{data.name}</DialogTitle>
+                    <DialogDescription>
+                      This action cannot be undone. This will permanently delete
+                      your account and remove your data from our servers.
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+            ))}
+            <div className="w-full">
+              <div
+                onClick={() => {
+                  router.push("/architectures/add-new");
+                }}
+                className="border-2 border-dashed border-gray-400 rounded-xl p-6 flex items-center justify-center cursor-pointer h-full hover:bg-gray-50"
+              >
+                <div className="text-center">
+                  <p className="text-lg font-semibold text-gray-600">
+                    + Add New Architecture
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-}
+        )}
       </div>
     </div>
   );
