1963e929df3c395cae735ec0753fb89af4057826
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-07-03 06:51:28 +0530

    1. updated structure

--- app/(playground)/architectures/components/architecture-card.tsx
+++ app/(playground)/architectures/components/architecture-card.tsx
@@ -12,9 +12,10 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardDescription, CardTitle, CardAction, CardFooter } from "@/components/ui/card";
 import { Architecture } from "@/lib/schema";
 import { cn } from "@/lib/utils";
-import { IconTrash } from "@tabler/icons-react";
+import { IconTrash, IconTrendingUp } from "@tabler/icons-react";
 import axios from "axios";
 import React from "react";
 import { toast } from "sonner";
@@ -22,9 +23,9 @@ import { toast } from "sonner";
 const ArchitectureCard = ({data,onDelete,}: {data: Architecture, onDelete: () => void;}) => {
   return (
     <>
-      <div
+      {/* <div
         className={cn(
-          "bg-card text-card-foreground flex flex-col gap-3 rounded-xl border py-4 px-4 shadow-sm h-full justify-between cursor-pointer"
+          "bg-card text-card-foreground flex flex-col gap-3 rounded-xl border py-4 px-4 shadow-sm h-full  cursor-pointer"
         )}
       >
         <div className=" flex w-full">
@@ -32,7 +33,7 @@ const ArchitectureCard = ({data,onDelete,}: {data: Architecture, onDelete: () =>
             {data.name}
           </p>
         </div>
-        <div className="w-full flex">
+        <div className="w-full flex items-start">
           <p className=" text-muted-foreground text-sm line-clamp-3">
             {data.description}
           </p>
@@ -86,7 +87,31 @@ const ArchitectureCard = ({data,onDelete,}: {data: Architecture, onDelete: () =>
             </AlertDialog>
           )}
         </div>
-      </div>
+      </div> */}
+
+
+            <Card className="@container/card">
+        <CardHeader>
+          <CardDescription>Total Revenue</CardDescription>
+          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            $1,250.00
+          </CardTitle>
+          <CardAction>
+            <Badge variant="outline">
+              <IconTrendingUp />
+              +12.5%
+            </Badge>
+          </CardAction>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1.5 text-sm">
+          <div className="line-clamp-1 flex gap-2 font-medium">
+            Trending up this month <IconTrendingUp className="size-4" />
+          </div>
+          <div className="text-muted-foreground">
+            Visitors for the last 6 months
+          </div>
+        </CardFooter>
+      </Card>
     </>
   );
 };
