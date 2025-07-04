5c0508c921f2debb7d7d6f268bf9871053cc77c2
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-07-04 21:56:15 +0530

    1. get architecture for edit implemented

--- app/api/v1/architecture/[...slug]/route.ts
+++ app/api/v1/architecture/[...slug]/route.ts
@@ -41,3 +41,39 @@ export async function DELETE(req: Request, { params }: { params: Promise<Params>
         return NextResponse.json({ error: "Invalid url format." }, { status: 400 });
     }
 }
+
+
+
+export async function GET(req: Request, { params }: { params: Promise<Params> }) {
+    try {
+        await connectMongo();
+    }
+    catch (error) {
+        return NextResponse.json({ error: "MongoDB connection failed." }, { status: 500 });
+    }
+
+    const { slug } = await params;
+
+    if (slug.length === 1) {
+        const session = await getServerSession(authOptions);
+        if (session?.user) {
+            try {
+                const doc = await ArchitectureModel.findOne({ name: slug[0], type: "custom", userId: session?.user.id });
+
+                if (!doc) {
+                    return NextResponse.json({ error: "Architecture not found." }, { status: 404 });
+                }
+                return NextResponse.json(doc, {status:200}); 
+            }
+            catch (error) {
+                return NextResponse.json({ error: "Failed to delete architecture." }, { status: 500 });
+            }
+        }
+        else {
+            return NextResponse.json({ error: "Unauthorized." }, { status: 401 })
+        }
+    }
+    else {
+        return NextResponse.json({ error: "Invalid url format." }, { status: 400 });
+    }
+}
