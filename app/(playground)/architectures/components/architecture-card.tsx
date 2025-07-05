015235936869b08cc4d44591c772986f1ffceb6f
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-07-05 16:49:48 +0530

    1. button bug fixed

--- app/(playground)/architectures/components/architecture-card.tsx
+++ app/(playground)/architectures/components/architecture-card.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -12,17 +12,22 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardDescription, CardTitle, CardAction, CardFooter } from "@/components/ui/card";
 import { Architecture } from "@/lib/schema";
 import { cn } from "@/lib/utils";
-import { IconEdit, IconTrash, IconTrendingUp } from "@tabler/icons-react";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { toast } from "sonner";
 
-const ArchitectureCard = ({data,onDelete,}: {data: Architecture, onDelete: () => void;}) => {
-  const router = useRouter()
+const ArchitectureCard = ({
+  data,
+  onDelete,
+}: {
+  data: Architecture;
+  onDelete: () => void;
+}) => {
+  const router = useRouter();
   return (
     <>
       <div
@@ -40,73 +45,79 @@ const ArchitectureCard = ({data,onDelete,}: {data: Architecture, onDelete: () =>
             {data.description}
           </p>
         </div>
-              <div className=" flex w-full justify-between items-end">
+        <div className=" flex w-full justify-between items-end">
           <Badge variant={"outline"} className="font-light">
             {data.type.charAt(0).toUpperCase() + data.type.slice(1)}
           </Badge>
 
-<div className=" flex gap-2">
-                  <Button
-                  className="w-8 h-8 cursor-pointer hover:scale-110"
-                  variant={"outline"}
-                  onClick={(e) => {e.stopPropagation() 
-                    router.push(`/architectures/edit/${data.name}`)
-                  }}
-                >
-                  {" "}
-                  <IconEdit />{" "}
-                </Button>
+          <div className=" flex gap-2">
+            {data.type === "custom" && (
+            <Button
+              className="w-8 h-8 cursor-pointer hover:scale-110"
+              variant={"outline"}
+              onClick={(e) => {
+                e.stopPropagation();
+                router.push(`/architectures/edit/${data.name}`);
+              }}
+            >
+              {" "}
+              <IconEdit />{" "}
+            </Button> )}
 
-          {data.type === "custom" && (
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button
-                  className="w-8 h-8 cursor-pointer hover:scale-110"
-                  variant={"destructive"}
-                  onClick={(e) => e.stopPropagation()}
-                >
-                  {" "}
-                  <IconTrash />{" "}
-                </Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    This action cannot be undone. Once delete you cannot recover
-                    the deleted architecture.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel className=" cursor-pointer">
-                    Cancel
-                  </AlertDialogCancel>
-                  <AlertDialogAction
-                    className="bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 cursor-pointer"
-                    onClick={() => {
-                      axios
-                        .delete(`/api/v1/architecture/${data._id}`)
-                        .then((res) => {
-                          toast("Deleted successfully");
-                          onDelete();
-                        })
-                        .catch((err) => {
-                          console.log(err)
-                          toast("Something went wrong");
-                        });
-                    }}
+            {data.type === "custom" && (
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button
+                    className="w-8 h-8 cursor-pointer hover:scale-110"
+                    variant={"destructive"}
+                    onClick={(e) => e.stopPropagation()}
                   >
-                    Delete
-                  </AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-            
-          )}
+                    {" "}
+                    <IconTrash />{" "}
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>
+                      Are you absolutely sure?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone. Once delete you cannot
+                      recover the deleted architecture.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel
+                      className=" cursor-pointer"
+                      onClick={(e) => e.stopPropagation()}
+                    >
+                      Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction
+                      className="bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 cursor-pointer"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        axios
+                          .delete(`/api/v1/architecture/${data._id}`)
+                          .then((res) => {
+                            toast("Deleted successfully");
+                            onDelete();
+                          })
+                          .catch((err) => {
+                            console.log(err);
+                            toast("Something went wrong");
+                          });
+                      }}
+                    >
+                      Delete
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            )}
           </div>
         </div>
       </div>
-      
     </>
   );
 };
